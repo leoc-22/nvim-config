8758801879065eee31c9958228585d979e8ae626
Leo Cao <lcao@tyro.com>
2025-02-18 13:04:34 +1100

    chore: add keymap for select all

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,6 +7,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 vim.keymap.set('n', '<leader>fb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>')
+vim.keymap.set('n', '<C-A>', 'ggVG', { noremap = true })
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
