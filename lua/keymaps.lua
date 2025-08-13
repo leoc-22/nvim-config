d73606af9cb12e01be77bce7c5fbdddc91bf2ed3
Leo Cao <lcao@tyro.com>
2025-08-13 10:29:44 +1000

    paste without yanking replaced text

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,8 @@
 -- default to open netrw
 -- vim.keymap.set('n', '<C-e>', ':Explore<CR>')
 
+vim.keymap.set('v', 'p', '"_dP', { desc = 'Paste without yanking replaced text' })
+
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
