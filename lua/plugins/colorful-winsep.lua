d2f21b5f609103276bc92f3800282ae57941a0af
Leo Cao <lcao@tyro.com>
2025-11-28 15:20:18 +1100

    configure colorful-winsep

--- lua/plugins/colorful-winsep.lua
+++ lua/plugins/colorful-winsep.lua
@@ -3,7 +3,10 @@ return {
   event = { "WinLeave" },
   config = function()
     require("colorful-winsep").setup({
-      border = "single",
+      border = "bold",
+      animate = {
+        enabled = false, -- false to disable, or choose a option below (e.g. "shift") and set option for it if needed
+      },
     })
   end,
 }
