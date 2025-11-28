8e78354bb39b10d9f46951901f19940d5aa79a9a
Leo Cao <lcao@tyro.com>
2025-11-28 15:16:59 +1100

    add colorful-winsep.lua

--- lua/plugins/colorful-winsep.lua
+++ lua/plugins/colorful-winsep.lua
@@ -1,5 +1,9 @@
 return {
   "nvim-zh/colorful-winsep.nvim",
-  config = true,
   event = { "WinLeave" },
+  config = function()
+    require("colorful-winsep").setup({
+      border = "single",
+    })
+  end,
 }
