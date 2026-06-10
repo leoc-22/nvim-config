b5492acc32181287917b6d54711b9f5e2911c069
Leo Cao <leoc.001re@gmail.com>
2026-06-10 21:53:18 +1000

    Add fff.nvim search plugin (#8)

---
+++ lua/plugins/fff.lua
@@ -0,0 +1,69 @@
+return {
+  {
+    'dmtrKovalenko/fff.nvim',
+    build = function()
+      require('fff.download').download_or_build_binary()
+    end,
+    lazy = false,
+    opts = {
+      prompt_vim_mode = false,
+      layout = {
+        height = 0.9,
+        width = 0.7,
+        prompt_position = 'top', -- or 'top'
+        preview_position = 'bottom',
+        preview_size = 0.7,
+      },
+      preview = {
+        line_numbers = true,
+        wrap_lines = true,
+        filetypes = {
+          markdown = { wrap_lines = true },
+          text = { wrap_lines = true },
+        },
+      },
+      git = {
+        status_text_color = true,
+      },
+    },
+    keys = {
+      {
+        '<leader>ff',
+        function()
+          require('fff').find_files()
+        end,
+        desc = '[F]FF [F]iles',
+      },
+      {
+        '<leader>fg',
+        function()
+          require('fff').live_grep()
+        end,
+        desc = '[F]FF [G]rep',
+      },
+      {
+        '<leader>fc',
+        function()
+          require('fff').live_grep { query = vim.fn.expand '<cword>' }
+        end,
+        desc = '[F]FF [C]urrent word',
+      },
+      {
+        '<leader>fz',
+        function()
+          require('fff').live_grep { grep = { modes = { 'fuzzy', 'plain' } } }
+        end,
+        desc = '[F]FF fu[Z]zy grep',
+      },
+      {
+        '<leader>fr',
+        function()
+          require('fff').scan_files()
+        end,
+        desc = '[F]FF [R]escan files',
+      },
+    },
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
