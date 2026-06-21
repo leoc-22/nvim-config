14c76d8b3de8879fc396bd0cace04846def96cfc
Leo Cao <leoc.001re@gmail.com>
2026-06-21 22:23:17 +1000

    [codex] Use Glance for LSP references (#11)
    * Use Glance for LSP references
    
    * Remove Telescope LSP picker

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -9,7 +9,7 @@ require('lazy').setup({
   require 'plugins.gitsigns',
   require 'plugins.which-key',
   require 'plugins.fff',
-  require 'plugins.telescope',
+  require 'plugins.glance',
   require 'plugins.lspconfig',
   require 'plugins.conform',
   require 'plugins.catppuccin',
